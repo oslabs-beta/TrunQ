e75b9c2b4afca75a41a4ee25d38819cdb5667e50
Brian Haller <brian@brianhaller.com>
2019-10-29 20:59:07 -0400

    fleshing out demo react app for thursday mvp presentation

--- components/App.jsx
+++ components/App.jsx
@@ -1,14 +1,48 @@
 import React, { Component } from 'react'
+import PokeCard from './PokeCard.jsx'
 
 class App extends Component {
     constructor(props) {
         super(props);
-
+        this.query = `query {
+                    pokemon(name: "Pikachu") {
+                      name
+                      image
+                      attacks {
+                        special {
+                          name
+                        }
+                      }
+                      evolutions {
+                          name
+                      }
+                    }
+                  }`
+        this.state = {
+            pokeInfo: null
+        }
+        this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick () {
+        fetch('https://graphql-pokemon.now.sh/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({query: this.query})
+        })
+        .then(res => res.json())
+        .then(info => {
+            this.setState({pokeInfo: info})
+        })
+    }
+
     render() {
         return (
             <div>
-                <button>WOW I ROCK</button>
+                <button onClick={() => this.handleClick()}>WOW I ROCK</button>
+                <PokeCard pokeinfo={this.state.pokeInfo} />
             </div>
         )
     }
---
+++ components/PokeCard.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const PokeCard = props => {
+    return (
+        <div style={{border: "1px solid black"}}>
+            Hello
+        </div>
+    )
+}
+
+export default PokeCard
