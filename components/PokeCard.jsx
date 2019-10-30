8d54b6f87933a2529deb85060e504660c0fc872a
Ben Ray <benray887@gmail.com>
2019-10-30 08:24:59 -0700

    adding art container

--- components/PokeCard.jsx
+++ components/PokeCard.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react'
 
 const PokeCard = props => {
+    console.log(props.pokeInfo.data.pokemon)
+    let evolutions = ''
+    if (props.pokeInfo.data.pokemon.evolutions) {
+        evolutions = props.pokeInfo.data.pokemon.evolutions[0].name
+    }
     return (
         <div style={{border: "3px solid black", height: "200px", width: "200px"}}>
             <h1>{props.pokeInfo.data.pokemon.name}</h1>
             
             <img src={props.pokeInfo.data.pokemon.image} style={{height: 50, width: 50}} />
-            {/* <p>{props.pokeInfo.data.pokemon.attacks.special[0]}</p> */}
+            <p>Evolutions {evolutions}</p>
             <p>Time to Fetch: {props.fetchTime} ms</p>
+
+
         </div>
     )
 }
---
+++ components/PokeContainer.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from 'react'
+import PokeCard from './PokeCard.jsx'
+
+class PokeContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            pokeInfo: [],
+            pokeName: null, 
+            fetchTime: [],
+            evolutionBool: false
+        }
+        this.handleClick = this.handleClick.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.pokeQueryBuilder = this.pokeQueryBuilder.bind(this);
+        this.handleTruth = this.handleTruth.bind(this)
+
+    }
+
+    handleClick (event) {
+        event.preventDefault()
+        const query = this.pokeQueryBuilder(this.state.pokeName, this.state.evolutionBool);
+        let startTime = Date.now(); 
+        fetch('https://graphql-pokemon.now.sh/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({query: query})
+        })
+        .then(res => res.json())
+        .then(info => {
+            let pokeArray = [...this.state.pokeInfo, info]
+            let elapsedTime = Date.now() - startTime;
+            let timeArray = [...this.state.fetchTime, elapsedTime];
+            this.setState({ pokeInfo: pokeArray, fetchTime: timeArray })
+        })
+    }
+
+    handleNameChange (e) {
+        this.setState({pokeName: e.target.value});
+    }
+
+    handleTruth () {
+        let truth
+        if (this.state.evolutionBool) truth = false
+        else {truth = true}
+        this.setState({evolutionBool: truth})
+        console.log(this.state.evolutionBool)
+    }
+
+    pokeQueryBuilder (pokeName, evolutions = false) {
+        let query = `query {
+                    pokemon(name: "${pokeName}") {
+                      name
+                      image
+                      attacks {
+                        special {
+                          name
+                        }
+                      }`
+
+            if (evolutions) {
+                query +=(`
+                evolutions {
+                    name
+                }
+              }
+          }`)
+            }
+            else{
+                query +=(
+                  `  }
+                }`  
+                )
+            }
+        console.log(query)
+        return query        
+    }
+
+    render() {
+        const pokeCards = []
+        for (let i = 0; i < this.state.pokeInfo.length; i += 1) {
+            pokeCards.push(<PokeCard pokeInfo={this.state.pokeInfo[i]} fetchTime={this.state.fetchTime[i]}/>)
+        }
+        return (
+            <div className = 'pokeContainer'>
+                <form>
+                    <input id="pokeName" value={this.state.pokeName} onChange={this.handleNameChange} type="text" />
+                    <button onClick={(event) => this.handleClick(event)}>QUERY POKEMON NAME</button>
+                    <input type='checkbox' onChange={this.handleTruth}></input>
+                </form>
+                    {pokeCards}
+            </div>
+        )
+    }
+}
+
+export default PokeContainer;
