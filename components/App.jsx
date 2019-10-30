345153d8fa1e0436e3df9733c0db9774cda03de7
Brian Haller <brian@brianhaller.com>
2019-10-29 21:58:31 -0400

    timer implemented on react demo

--- components/App.jsx
+++ components/App.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react'
 import PokeCard from './PokeCard.jsx'
+import { findDeprecatedUsages } from 'graphql';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.query = `query {
-                    pokemon(name: "Pikachu") {
+        this.state = {
+            pokeInfo: [],
+            pokeName: null, 
+            fetchTime: []   
+        }
+        this.handleClick = this.handleClick.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.queryBuilder = this.queryBuilder.bind(this);
+
+    }
+
+    handleClick (event) {
+        event.preventDefault()
+        const query = this.queryBuilder(this.state.pokeName);
+        let startTime = Date.now(); 
+        fetch('https://graphql-pokemon.now.sh/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({query: query})
+        })
+        .then(res => res.json())
+        .then(info => {
+            let pokeArray = [...this.state.pokeInfo, info]
+            let elapsedTime = Date.now() - startTime;
+            let timeArray = [...this.state.fetchTime, elapsedTime];
+            this.setState({ pokeInfo: pokeArray, fetchTime: timeArray })
+        })
+    }
+
+    handleNameChange (e) {
+        this.setState({pokeName: e.target.value});
+    }
+
+    queryBuilder (pokeName) {
+        return  `query {
+                    pokemon(name: "${pokeName}") {
                       name
                       image
                       attacks {
@@ -17,32 +54,21 @@ class App extends Component {
                           name
                       }
                     }
-                  }`
-        this.state = {
-            pokeInfo: null
-        }
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick () {
-        fetch('https://graphql-pokemon.now.sh/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({query: this.query})
-        })
-        .then(res => res.json())
-        .then(info => {
-            this.setState({pokeInfo: info})
-        })
+                }`
     }
 
     render() {
+        const pokeCards = []
+        for (let i = 0; i < this.state.pokeInfo.length; i += 1) {
+            pokeCards.push(<PokeCard pokeInfo={this.state.pokeInfo[i]} fetchTime={this.state.fetchTime[i]}/>)
+        }
         return (
             <div>
-                <button onClick={() => this.handleClick()}>WOW I ROCK</button>
-                <PokeCard pokeinfo={this.state.pokeInfo} />
+                <form>
+                    <input id="pokeName" value={this.state.pokeName} onChange={this.handleNameChange} type="text" />
+                    <button onClick={(event) => this.handleClick(event)}>QUERY POKEMON NAME</button>
+                </form>
+                    {pokeCards}
             </div>
         )
     }
--- components/PokeCard.jsx
+++ components/PokeCard.jsx
@@ -2,8 +2,12 @@ import React, { Component } from 'react'
 
 const PokeCard = props => {
     return (
-        <div style={{border: "1px solid black"}}>
-            Hello
+        <div style={{border: "3px solid black", height: "200px", width: "200px"}}>
+            <h1>{props.pokeInfo.data.pokemon.name}</h1>
+            
+            <img src={props.pokeInfo.data.pokemon.image} style={{height: 50, width: 50}} />
+            {/* <p>{props.pokeInfo.data.pokemon.attacks.special[0]}</p> */}
+            <p>Time to Fetch: {props.fetchTime} ms</p>
         </div>
     )
 }
