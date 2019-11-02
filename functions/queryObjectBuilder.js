930dc1d933a6008542ebfe5498b9501111e2c28c
Brian Haller <brian@brianhaller.com>
2019-11-01 21:24:31 -0400

    need to add helper function to assist in recursing

--- functions/queryObjectBuilder.js
+++ functions/queryObjectBuilder.js
@@ -2,6 +2,10 @@ const parser = require('./parser')
 let tester = parser.parseVariables('artist(id: "mark-rothko")', ['id', 'name'], ['size'])
 
 //the goal of trunqify variables is to take the variables out of the parenthesis and turn it into the appropriate object
+
+
+
+
 let trunqifyVariables = (parserOutput) => {
     //start with an empty object
     let trunQVariables = {}
@@ -18,10 +22,67 @@ let trunqifyVariables = (parserOutput) => {
     return trunQVariables
 }
 
+let searchTrunQ = (trunQVariables, limits) => {
+    let trunQNum = 0;
+    limits.forEach(limit => {
+        if(trunQVariables[limit]) trunQNum = trunQVariables[limit]
+    })
+    return trunQNum 
+}
+
+function innerTrunQify (output, levels, trunQSize, keysArr, i, uniques, limits, latestQuery) {
+    let curr = keysArr[++i]
+    console.log(curr, i)
+    let dummyObj = output
+    for (let z = 0; z < levels.length; z += 1) {
+        dummyObj = dummyObj[levels[z]];
+    }
+    for (let k = 0; k < trunQSize; k += 1) {
+        let temp = ''
+        let tempObj = {}
+        let innertrunQSize = 0;
+        for (let j = 0; j < curr.length; j += 1) {
+            if (curr[j] === ' ') {
+                tempObj[temp] = {}
+                latestQuery = temp
+                temp = ''
+            }
+            else if(curr[j] === '(') {
+
+                let parsedArr = parser.parseVariables(curr, uniques, limits)
+                let trunQVars = trunqifyVariables(parsedArr)
+                tempObj[latestQuery].trunQVariables = trunQVars
+                //right here - add trunQarray based on size
+                    //if within trunqvariables exists a limits variable
+                    //create a trunqArray of equivalent size feeding it the correct
+                innertrunQSize = searchTrunQ(trunQVars, limits);
+                // console.log("TRUNQSIZE", trunQSize)
+                j = curr.length + 1
+                levels.push(latestQuery)
+                if (innertrunQSize > 0) {
+                    tempObj[latestQuery].trunQLimits = []
+                    i = innerTrunQify (tempObj[latestQuery], levels, innertrunQSize, keysArr[++i], uniques, limits, latestQuery)
+                    trunQSize = 0;
+                }
+                //maybe run another iteration of this if innertrunqsize is greater than 0
+            }
+            else {
+                temp += curr[j]
+            }
+            if(j === curr.length-1) {
+                tempObj[temp] = {}
+            }
+        }
+        dummyObj.trunQLimits.push(tempObj)
+    }
+    return i;
+}
+
 let input = [ 
     { 'artist(id: "mark-rothko")': 1,
         'name artworks (name: "chapel" size: 2)': 2,
-            'id imageUrl': 3,
+            'id imageUrl (size: 2)': 3,
+                'hello': 4,
         bullshit: 2 
     },
 
@@ -34,6 +95,7 @@ let fieldsToObject = (arr, uniques=[], limits = []) => {
     let output = {}
     let levels = []
     let previousLevel = 0;
+    let trunQSize = 0;
 
     for (let z =0; z<arr.length; z++) {
         let latestQuery = ''
@@ -42,7 +104,7 @@ let fieldsToObject = (arr, uniques=[], limits = []) => {
         let dummyObj = output
 
         for (let i=0; i<keysArr.length; i++) {
-            console.log('iteration', i)
+            // console.log('iteration', i)
             let curr = keysArr[i]
             dummyObj = output;
             // console.log(curr);
@@ -53,9 +115,11 @@ let fieldsToObject = (arr, uniques=[], limits = []) => {
                     let uniqueKey = parsedArr[0];
                     output[uniqueKey] = {};
                     levels.push(uniqueKey);
-                    output[uniqueKey].trunQVariables = trunqifyVariables(parsedArr);
+                    let trunQVars = trunqifyVariables(parsedArr);
+                    output[uniqueKey].trunQVariables = trunQVars
 //right here - add trunQarray based on size
-
+                    trunQSize = searchTrunQ(trunQVars, limits);
+                    // console.log("TRUNQSIZE", trunQSize)
                     output[uniqueKey][parsedArr[1].query] = {};
                     levels.push(parsedArr[1].query);
                     
@@ -68,12 +132,9 @@ let fieldsToObject = (arr, uniques=[], limits = []) => {
                     levels.push(curr);
                 }
             }
-            else if (input[curr] > 1) {
+            else if (input[curr] > 1 && trunQSize === 0) {
                 //if prevlevel is greater than pop
-
                 if (previousLevel > input[curr]) levels.pop();
-                let parensReg =/\(([^()]*)\)/
-                let objectReferencefromArray = levels[input[curr] - 1]
                 let temp = ''
                 for (let i = 0; i < levels.length; i += 1) {
                     // console.log("LEVELS IN LOOP", levels[i])
@@ -88,33 +149,46 @@ let fieldsToObject = (arr, uniques=[], limits = []) => {
                         temp = ''
                     }
                     else if(curr[j] === '(') {
+
                         let parsedArr = parser.parseVariables(curr, uniques, limits)
-                        dummyObj[latestQuery].trunQVariables = trunqifyVariables(parsedArr)
+                        let trunQVars = trunqifyVariables(parsedArr)
+                        dummyObj[latestQuery].trunQVariables = trunQVars
                         //right here - add trunQarray based on size
                             //if within trunqvariables exists a limits variable
                             //create a trunqArray of equivalent size feeding it the correct
-                        j = curr.length+1
+                        trunQSize = searchTrunQ(trunQVars, limits);
+                        // console.log("TRUNQSIZE", trunQSize)
+                        if (trunQSize > 0) dummyObj[latestQuery].trunQLimits = []
+                        j = curr.length + 1
                         levels.push(latestQuery)
                     }
                     else {
                         temp += curr[j]
                     }
-
                     if(j === curr.length-1) {
                         dummyObj[temp] = {}
                     }
                 }
                 // console.log("dummy", levels[levels.length-1], dummyObj);
             }
-            // console.log("OUTPUT", output);
-            console.log(levels)
-
-            previousLevel = input[curr];
+            else if (input[curr] === 1 && trunQSize > 0) {
+                
+            }
+            else if (input[curr] > 1 && trunQSize > 0) {
+                i = innerTrunQify (output, levels, trunQSize, keysArr, i, uniques, limits, latestQuery)
+                trunQSize = 0;
+                
+            }
         }
+            // console.log("OUTPUT", output);
+            // console.log(levels)
+            // previousLevel = input[curr];
         levels=[]
     }
-    return output
+    return output["artist-mark-rothko"].artist.artworks
 }
+
+
 
 let test = fieldsToObject(input, ['id', 'name'], ['size'])
 console.log(test)
@@ -135,6 +209,7 @@ const emptyCacheObjFromQuery2 = {
                 trunQarrays: 
                     [{id: {}, imageUrl1: {}},
                     {id2: {}, imageUrl2: {}}]
+                
             },
             bullshit: {}
         }
