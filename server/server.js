c138584deb486b1e6133657ae7b3979476697581
Mike Evans <amike.evans@gmail.com>
2019-11-14 11:00:23 -0500

    temp

--- server/server.js
+++ server/server.js
@@ -5,19 +5,13 @@ exports.port = 3000;
 const bodyParser = require('body-parser')
 const TrunQServer = require('./TrunQServer');
 const trunQBack = new TrunQServer('https://graphql-pokemon.now.sh/')
-// const trunQBack2 = new TrunQStern('https://metaphysics-production.artsy.net')
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.use('/graphql', trunQBack.getAllData, (req, res, next) => {
-    res.status(200).json(trunQBack.data);
+  res.status(200).json(trunQBack.data);
 })
-
-// ******** uncomment to test with metaphysics API *******
-// app.use('/artGraphQL', trunQBack2.getAllData, (req, res, next) => {
-//     res.status(200).json(trunQBack.data);
-// })
 
 module.exports = app;
