a5498e56fee44d647d4f9b41e82d17c535d92c68
Mike Evans <amike.evans@gmail.com>
2019-11-14 10:47:31 -0500

    server.js purge

--- server/server.js
+++ server/server.js
@@ -3,37 +3,21 @@ const path = require('path')
 const app = express();
 exports.port = 3000;
 const bodyParser = require('body-parser')
-
-// EXAMPLE TRUNQ BACKEND SETUP *******************
-
 const TrunQServer = require('./TrunQServer');
-
-// create instance of TrunQStern
-// args: graphQl endpoint, redis client
 const trunQBack = new TrunQServer('https://graphql-pokemon.now.sh/')
 // const trunQBack2 = new TrunQStern('https://metaphysics-production.artsy.net')
-
-// ***********************************************
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
-// TRUNQ BACKEND EXAMPLE ROUTE *******************
-
 app.use('/graphql', trunQBack.getAllData, (req, res, next) => {
     res.status(200).json(trunQBack.data);
 })
 
+// ******** uncomment to test with metaphysics API *******
 // app.use('/artGraphQL', trunQBack2.getAllData, (req, res, next) => {
-//     console.log('5 **** that.data before response to client: ', trunQBack.data);
 //     res.status(200).json(trunQBack.data);
-//     // res.send(res.locals.message);
 // })
-
-// ***********************************************
-
-
-// EXPORT APP FOR TESTING VS PRODUCTION/DEMO *****
 
 module.exports = app;
